e4fe682067de3205f6175aa2fa4ef30f32541be0
mosdef71 <krag71@mail.ru>
2021-08-30 15:33:01 +0300

    feat: add solution for test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function heck(str, bracketsConfig){
+  const OPEN_BRACKETS = ["(", "[", "{"];
+  const BRACKETS_DICT = {
+    ")" : "(",
+    "}" : "}",
+    ")" : "(",
+    "|" : "|",
+  };
+  let stack = [];
+  let consolidStr = [str, bracketsConfig].flat(Infinity).join("");
+
+  for (let i = 0; i < consolidStr.length; i++){
+    let currSymbol = consolidStr[i];
+    if (OPEN_BRACKETS.includes(currSymbol)){
+      stack.push(currSymbol);
+    } else {
+        if (stack.length === 0) {
+          return false;
+        }
+        let lastSymbol = stack[stack.length - 1];
+        if (BRACKETS_DICT[currSymbol] === lastSymbol) {
+          stack.pop();
+        } else { 
+          return false
+        }
+    }
+  }
+  return stack.length === 0;
 }
